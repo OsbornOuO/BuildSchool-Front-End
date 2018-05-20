eb04bd1b09a778ab647f03e3d48dfdc5597bf653
OsbornOuO <osbornoops@gmail.com>
2018-05-20 18:16:30 +0800

    修改資料檔檔名 新增個人網站

--- TypeScript/Sample001/js/1.js
+++
@@ -1,8 +0,0 @@
-var ar = [10, 20, 30, 40, 50];
-for (var i = 0; i < ar.length; i++) {
-    for (var i_1 = 0; i_1 < 9; i_1++) {
-        console.log(i_1);
-    }
-    console.log(i, ar[i]);
-}
-//# sourceMappingURL=1.js.map
